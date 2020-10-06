94aa0c27908d1ccba36cf90a40ed094c4150ee30
zeta <zezeon1@gmail.com>
2020-10-06 23:21:29 +0900

    2020. 10. 06. (화) 23:21:29 KST autocommit

---
+++ a.c
@@ -0,0 +1,9 @@
+#include<emscripten.h>
+#include<iostream>
+using namespace std;
+
+extern "C" int main()
+{
+	cout << "hello" << endl;
+}
+
---
+++ c.cpp
@@ -0,0 +1,12 @@
+#include<string>
+#include<iostream>
+using namespace std;
+
+string f() {
+	cout << "Printing" << endl;
+	return "Hello";
+}
+int main()
+{
+	f();
+}
